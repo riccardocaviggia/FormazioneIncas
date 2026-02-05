d4bd86ed82378fac30cad59033a9fbb7b91533e0
riccardocaviggia <riccardocaviggia@gmail.com>
2026-02-05 15:58:04 +0100

    Ex.15 completed.

--- Queries/Ex.15.sql
+++ Queries/Ex.15.sql
@@ -29,12 +29,51 @@ alter table Customers add IsActive bit not null default 1
 alter table Customers add constraint UQ_CompanyName UNIQUE (CompanyName)
 
 -- Crea una vista che unisce gli ordini con i relativi nomi dei clienti.
-
+go
+create view V_GetRelationClientOrders as select o.OrderID, c.CompanyName, o.OrderDate, o.ShipCity from Orders o join Customers c on o.CustomerID = c.CustomerID
+go
+select * from V_GetRelationClientOrders
+go
 
 -- Crea una stored procedure che, dato un ID cliente, restituisca i suoi ordini.
+go
+create procedure GetClientOrders @CustomerID nchar(5) 
+as begin
+select * from Orders where CustomerID = @CustomerID
+end;
+go
+
+exec GetClientOrders @CustomerID = 
 
 
 -- Crea una funzione che calcoli uno sconto del 10% su un importo superiore a 100.
+go
+create function dbo.SaleCalculator(@Total decimal(5,2)) returns decimal(5,2)
+as begin
+	declare @Sale decimal(5,2)
+	set @Sale = case when @Total > 100 then @Total * 0.1 else 0 end
+	return @Sale
+end;
+go
 
+exec dbo.SaleCalculator @Total = 150
+select dbo.SaleCalculator(150) as discount
 
 -- Crea un trigger che registri in una tabella di log ogni nuovo ordine inserito.
+go
+create table OrdersLog (
+	LogID int primary key identity,
+	OrderID int,
+	OrderDate datetime default getDate(),
+	foreign key(OrderID) references Orders(OrderID)
+	)
+go
+
+create trigger trg_CreateLogOrder
+on Orders
+after insert
+as
+begin
+	insert into OrdersLog(OrderID)
+	select OrderID from inserted
+end;
