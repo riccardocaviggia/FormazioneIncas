7a93e0aa09423c8953c0d20ae035d9970ca01989
riccardocaviggia <riccardocaviggia@gmail.com>
2026-02-10 14:43:53 +0100

    Ex.19 query 3 corrected.

--- 19_EcommerceSql/Ex19query3.sql
+++ 19_EcommerceSql/Ex19query3.sql
@@ -1,6 +1,6 @@
 use EcommerceDB
 
-select p.ProductName, sum(od.ProductQuantity) as product_quantity
+select p.ProductName, sum(od.ProductQuantity) as products_sold
 from Products p
 join Order_Details od on od.ProductID = p.ProductID
 group by p.ProductName
