78785833048b0f341aa5b11f3d9f83f9fb5871e4
riccardocaviggia <riccardocaviggia@gmail.com>
2026-02-12 10:17:58 +0100

    Ex.21.4 query 2 formatted.

--- 21_SQL/21.4_Bank/Ex.21.4query2.sql
+++ 21_SQL/21.4_Bank/Ex.21.4query2.sql
@@ -1,5 +1,11 @@
-use BankDB
-select c.ClientName, c.ClientSurname, sum(a.Balance) as total_balance
-from Clients c
-join Accounts a on a.ClientID = c.ClientID
-group by c.ClientName, c.ClientSurname
+use BankDB 
+select 
+  c.ClientName, 
+  c.ClientSurname, 
+  sum(a.Balance) as total_balance 
+from 
+  Clients c 
+  join Accounts a on a.ClientID = c.ClientID 
+group by 
+  c.ClientName, 
+  c.ClientSurname
